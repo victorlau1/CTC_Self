2d7a8d1c7de35539f2afa9a7200a0de74acc4a96
Victor Lau <lau.victor.w@gmail.com>
2017-12-11 21:17:46 -0800

    finish linked list

--- src/chapter2/ch2-q4.js
+++ src/chapter2/ch2-q4.js
@@ -11,5 +11,42 @@
  * list is being manipulated.
  */
 export function partition(list, val) {
+    function nodeMaker(val){
+      this.val = val
+      this.next = null;
+    }
+
+    if (!list){
+      throw new Error('invalid list')
+    }
+
+    var currentNode = list;
   
+    var beforeNode, afterNode, startBNode, startANode;
+    while (currentNode) {
+      
+      if (beforeNode === undefined && currentNode.val < val){
+        beforeNode = new nodeMaker(currentNode.val);
+        startBNode = beforeNode;
+      } else if (afterNode === undefined && currentNode.val >= val){
+        afterNode = new nodeMaker(currentNode.val);
+        startANode = afterNode;
+      } else if (currentNode.val < val){
+        beforeNode.next = new nodeMaker(currentNode.val);
+        beforeNode = beforeNode.next;
+      } else {
+        afterNode.next = new nodeMaker(currentNode.val);
+        afterNode = afterNode.next;
+      }
+      currentNode = currentNode.next;
+    }
+    
+    if (beforeNode){
+      beforeNode.next = startANode || null;
+      beforeNode = startBNode
+    } else if (afterNode) {
+      beforeNode = startANode;
+    }
+   
+    return beforeNode;
 }
